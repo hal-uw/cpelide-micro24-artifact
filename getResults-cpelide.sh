312a69cb6fd787c59136456ee165f1513c452758
Matt Sinclair <mattdsinclair.wisc@gmail.com>
2024-08-14 18:25:24 -0500

    Shell for results gathering script for artifact

---
+++ getResults-cpelide.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Gathers results for CPElide MICRO '24 artifact and emits in CSV format
+
+# Babelstream
+
+# Square
+
+# BFS
+
+# Backprop
+
+# Hotspot3D
+
+# Other benchmarks not included because of runtime for artifact, but concept
+# is same as the above
